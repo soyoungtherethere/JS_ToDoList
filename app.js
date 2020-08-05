359ddad84c9ab44d78d94d272b686e65cfcebaa4
soyoungtherethere <311syp@naver.com>
2020-08-05 09:44:25 -0700

    generate addTodo function

---
+++ app.js
@@ -0,0 +1,26 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+}
